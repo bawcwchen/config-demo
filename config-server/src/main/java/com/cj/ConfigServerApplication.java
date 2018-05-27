7c2b021f06af7708fc088425a715ef385f45754c
wb-cj189958 <wb-cj189958@alibaba-inc.com>
2018-05-27 18:57:18 +0800

    upload

---
+++ config-server/src/main/java/com/cj/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.cj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wb-cj189958
+ * @date 2018/5/26 22:40
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
